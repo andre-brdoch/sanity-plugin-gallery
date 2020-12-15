19ffd99642feecfdbd38c80d44185be55a35443a
Andre Brdoch <andrebrdoch@yahoo.de>
2020-12-15 19:36:54 +0100

    updated css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
   env: {
-    browser: true
+    browser: true,
   },
-  parser: "babel-eslint",
-  extends: "airbnb",
+  parser: 'babel-eslint',
+  extends: 'airbnb',
   rules: {
-    "arrow-parens": [2, "as-needed"],
-    "comma-dangle": [2, "always-multiline"],
-    "no-underscore-dangle": 0,
-    "import/no-unresolved": 0,
-    "react/destructuring-assignment": 0,
-    "react/jsx-filename-extension": 0,
-    "react/prefer-stateless-function": 0
-  }
+    'arrow-parens': [2, 'as-needed'],
+    'comma-dangle': [2, 'always-multiline'],
+    'no-underscore-dangle': 0,
+
+    'import/no-unresolved': 0,
+
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+    'react/destructuring-assignment': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+  },
 };
